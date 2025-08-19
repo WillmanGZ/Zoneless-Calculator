7b4a8cc16cae2ea24bc9ff071e0f6d5252809b3e
WillmanGZ <willmang@uninorte.edu.co>
2025-08-19 08:54:59 -0500

    fix(tests): update test configurations and improve test cases for app component

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Zoneless-Calculator');
+  it('should be 2', () => {
+    //A = Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    //A = Act
+    const result = num1 + num2;
+
+    //A = Assert
+    expect(result).toBe(3);
+  });
+
+  it('should render router outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
