9ccb41d74bac9ceb88ac92b0657a2b3199fba94f
WillmanGZ <willmang@uninorte.edu.co>
2025-08-21 09:27:27 -0500

    test(calculator): add additional unit tests for CalculatorButtonComponent

--- src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -32,10 +32,34 @@ describe('CalculatorViewComponent', () => {
   it('should apply w-2/4 doubleSize is false', () => {
     fixture.componentRef.setInput('isDoubleSize', true);
     fixture.detectChanges();
-    
+
     const hostCssClasses: string[] = compiled.classList.value.split(' ');
 
     expect(hostCssClasses).toContain('w-2/4');
     expect(component.isDoubleSize()).toBeTrue();
   });
+
+  it('should emit onClick when handleClick is called', () => {
+    //Spy
+    spyOn(component.onClick, 'emit');
+    component.handleClick();
+
+    expect(component.onClick.emit).toHaveBeenCalled();
+    expect(component.onClick.emit).toHaveBeenCalledWith('');
+  });
+
+  it(
+    'should set isPressed to true and then false when key boardPressStyle is called with a matching key'
+  ),
+    (done: DoneFn) => {
+      component.contentValue()!.nativeElement.innerText = '1';
+      component.keyboardPressedStyle('1');
+
+      expect(component.isPressed()).toBeTrue();
+
+      setTimeout(() => {
+        expect(component.isPressed()).toBeFalse();
+        done();
+      }, 101);
+    };
 });
